939442d7dc0b93e669998a0f50f6eafb4e2f2f2f
Helge Fredheim <helge.fredheim@nav.no>
2016-09-26 14:02:13 +0200

    Klargjøring av modul for npm. La til prepublish og compile

---
+++ js/index.js
@@ -0,0 +1,5 @@
+import ArbeidsgiversSykmeldingOpplysninger from './components/sykmeldingOpplysninger/ArbeidsgiversSykmeldingOpplysninger';
+import DineSykmeldingOpplysninger from './components/sykmeldingOpplysninger/DineSykmeldingOpplysninger';
+
+export { ArbeidsgiversSykmeldingOpplysninger };
+export { DineSykmeldingOpplysninger };
