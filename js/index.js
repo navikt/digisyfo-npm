b6aa7d985d61673e1577a5adf4fa71c17875376a
Helge Fredheim <helge.fredheim@nav.no>
2016-09-27 09:14:43 +0200

    LESS ligger nå i digisyfo-npm

--- js/index.js
+++ js/index.js
@@ -1,5 +1,27 @@
 import ArbeidsgiversSykmeldingOpplysninger from './components/sykmeldingOpplysninger/ArbeidsgiversSykmeldingOpplysninger';
+<<<<<<< HEAD
 import DineSykmeldingOpplysninger from './components/sykmeldingOpplysninger/DineSykmeldingOpplysninger';
 
 export { ArbeidsgiversSykmeldingOpplysninger };
 export { DineSykmeldingOpplysninger };
+=======
+export { ArbeidsgiversSykmeldingOpplysninger };
+
+import DineSykmeldingOpplysninger from './components/sykmeldingOpplysninger/DineSykmeldingOpplysninger';
+export { DineSykmeldingOpplysninger };
+
+import * as ledetekster from './ledetekster';
+export { ledetekster };
+
+import * as datoUtils from './utils/datoUtils';
+export { datoUtils };
+
+import * as dinSykmeldingUtils from './utils/dinSykmeldingUtils';
+export { dinSykmeldingUtils }
+
+import * as utils from './utils';
+export { utils };
+
+import Utvidbar from './components/Utvidbar';
+export { Utvidbar };
+>>>>>>> LESS ligger nå i digisyfo-npm
