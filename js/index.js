1b4b1064f8f4b825245ba49949d17f468850849a
Helge Fredheim <helge.fredheim@nav.no>
2016-10-11 13:17:45 +0200

    Eksporterer SykmeldingOpplysning og SykmeldingNokkelopplysning da de trengs av syfofront

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 export * from './components/sykmeldingOpplysninger/ArbeidsgiversSykmeldingOpplysninger';
 export * from './components/sykmeldingOpplysninger/DineSykmeldingOpplysninger';
+export * from './components/sykmeldingOpplysninger/SykmeldingOpplysning'
 export * from './ledetekster';
 export * from './utils';
 export * from './components/Utvidbar';
