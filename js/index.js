c3c19c946a2c93e958273cdafc8da243a4f5a1d7
Helge Fredheim <helge.fredheim@nav.no>
2016-11-08 13:15:08 +0100

    La tidslinjen over i digisyfo-npm

--- js/index.js
+++ js/index.js
@@ -14,10 +14,27 @@ export * from './utils';
 import Varselstripe from './components/Varselstripe';
 export { Varselstripe };
 
+export * from './actions/hendelser_actions';
 export * from './actions/ledetekster_actions';
+export * from './actions/tidslinjer_actions';
 
 import ledetekster from './reducers/ledetekster';
 export { ledetekster };
 
+import tidslinjer from './reducers/tidslinjer';
+export { tidslinjer };
+
 import ledeteksterSagas from './sagas/ledeteksterSagas';
 export { ledeteksterSagas };
+
+import tidslinjerSagas from './sagas/tidslinjerSagas';
+export { tidslinjerSagas };
+
+import Tidslinje from './components/tidslinje/Tidslinje';
+export { Tidslinje };
+
+import Radiofaner from './components/Radiofaner';
+export { Radiofaner };
+
+import Hjelpetekst from './components/Hjelpetekst';
+export { Hjelpetekst };
