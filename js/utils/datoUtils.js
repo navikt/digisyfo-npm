77ffdd62fc9f953e55cfa407506e236c171e1231
Knut Nygaard <knut.nygaard@nav.no>
2017-01-18 09:48:03 +0100

    Utvider datoutils slik at datoer i tekstformat på ISO-standarden virker

--- js/utils/datoUtils.js
+++ js/utils/datoUtils.js
@@ -1,30 +1,26 @@
+const erLocalDate = (dato) => { return dato.year && dato.monthValue && dato.dayOfMonth; };
+
 export const toDate = (dato) => {
-    return new Date(dato.year, dato.monthValue - 1, dato.dayOfMonth);
+    if (typeof dato === 'undefined' || dato === null) {
+        return null;
+    }
+    return erLocalDate(dato) ? new Date(dato.year, dato.monthValue - 1, dato.dayOfMonth) : new Date(dato);
 };
 
 export const toDatePrettyPrint = (dato) => {
     if (typeof dato === 'undefined' || dato === null) {
         return null;
     }
-    let dag = dato.dayOfMonth;
-    if (dag.toString().length === 1) {
-        dag = `0${dag}`;
-    }
-    let maned = dato.monthValue;
-    if (maned.toString().length === 1) {
-        maned = `0${maned}`;
-    }
 
-    return `${dag}.${maned}.${dato.year}`;
-};
+    const _dato = toDate(dato);
 
-const parseDate = (dato) => {
-    if (typeof dato === 'undefined' || dato === null) {
-        return null;
-    }
-    return new Date(dato.year, dato.monthValue - 1, dato.dayOfMonth);
+    const days = _dato.getDate() < 10 ? `0${_dato.getDate()}` : `${_dato.getDate()}`;
+    const months = _dato.getMonth() + 1 < 10 ? `0${_dato.getMonth() + 1}` : `${_dato.getMonth() + 1}`;
+    const years = _dato.getFullYear();
+
+    return `${days}.${months}.${years}`;
 };
 
 export function getDuration(from, to) {
-    return Math.round(Math.floor(parseDate(to) - parseDate(from)) / (1000 * 60 * 60 * 24)) + 1;
+    return Math.round(Math.floor(toDate(to) - toDate(from)) / (1000 * 60 * 60 * 24)) + 1;
 }
