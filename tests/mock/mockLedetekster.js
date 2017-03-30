9aabf977bd11f6e925b2f70da404c56ac4362406
Helge Fredheim <helge.fredheim@nav.no>
2017-03-30 10:54:12 +0200

    Manglet komma i mockLedetekster

--- tests/mock/mockLedetekster.js
+++ tests/mock/mockLedetekster.js
@@ -249,9 +249,9 @@ const ledetekster = {
     "sykepengesoknad.ferie-permisjon-utenlandsopphold.hatt-permisjon": "hatt permisjon",
     "din-sykmelding.diagnose.meta": "Diagnose blir ikke sendt videre til eventuell arbeidsgiver",
     "din-sykmelding.diagnosekode.hjelpetekst.tittel": "Diagnosekode",
-    "din-sykmelding.diagnosekode.hjelpetekst.tekst": "Diagnosekoden henviser til de internasjonale kodeverkene som klassifiserer sykdom og symptomer. De ulike diagnosekodene brukes for å gi en mest mulig presis diagnose."
+    "din-sykmelding.diagnosekode.hjelpetekst.tekst": "Diagnosekoden henviser til de internasjonale kodeverkene som klassifiserer sykdom og symptomer. De ulike diagnosekodene brukes for å gi en mest mulig presis diagnose.",
     "sykepengesoknad.angi-tid.normal-arbeidstimer.label-med-verdi": "%ANTALL% timer",
-    "din-sykmelding.diagnose.meta": "Diagnose blir ikke sendt videre til eventuell arbeidsgiver"
+    "din-sykmelding.diagnose.meta": "Diagnose blir ikke sendt videre til eventuell arbeidsgiver",
 };
 
 export default ledetekster;
