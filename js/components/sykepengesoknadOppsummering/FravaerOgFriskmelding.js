4d67bea64f18052247077251b47a82fc18d893d5
Helge Fredheim <helge.fredheim@bekk.no>
2017-09-22 12:29:08 +0200

    Bruker bare forrigeSykeforloepTom dersom del === 1
    SYFOUTV-854

--- js/components/sykepengesoknadOppsummering/FravaerOgFriskmelding.js
+++ js/components/sykepengesoknadOppsummering/FravaerOgFriskmelding.js
@@ -103,10 +103,11 @@ export const FeriePermisjonEllerUtenlandsopphold = ({ sykepengesoknad, ledetekst
         return aktivitet.periode;
     });
     const senesteTom = getTomDato(sykepengesoknad);
+    const fom = sykepengesoknad.del === 1 && sykepengesoknad.forrigeSykeforloepTom ? sykepengesoknad.forrigeSykeforloepTom : tidligsteFom(sykmeldingsperioder);
 
     return (<div className="js-feriePermisjonUtenlandsopphold oppsummering__bolk">
         <h3 className="oppsummering__sporsmal">{getLedetekst('sykepengesoknad.ferie-permisjon-utenlandsopphold.janei.sporsmal', ledetekster, {
-            '%FOM%': toDatePrettyPrint(sykepengesoknad.forrigeSykeforloepTom || tidligsteFom(sykmeldingsperioder)),
+            '%FOM%': toDatePrettyPrint(fom),
             '%TOM%': toDatePrettyPrint(senesteTom),
         })}</h3>
         <Avkrysset tekst={harHattFeriePermisjonEllerUtenlandsopphold ? getLedetekst('sykepengesoknad.ja', ledetekster) : getLedetekst('sykepengesoknad.nei', ledetekster)} />
